1f78982f63f856ace16b1a1586c77dd9b22b98b9
KeerthanaC38 <keerthananakka38@gmail.com>
2025-02-18 21:29:17 -0600

    Added custom messages for HttpResponses

--- src/main/java/com/receipt/processor/controller/ReceiptController.java
+++ src/main/java/com/receipt/processor/controller/ReceiptController.java
@@ -1,4 +1,5 @@
 package com.receipt.processor.controller;
+import com.receipt.processor.model.Item;
 import com.receipt.processor.model.Receipt;
 import com.receipt.processor.service.ReceiptService;
 import org.springframework.http.HttpStatus;
@@ -18,9 +19,21 @@ public class ReceiptController {
 
     @PostMapping("/receipts/process")
     public ResponseEntity<Map<String, String>> processReceipt(@RequestBody Receipt receipt) {
-        if (receipt.getRetailer() == null || receipt.getItems() == null || receipt.getItems().isEmpty()) {
+        if (receipt.getRetailer() == null || receipt.getRetailer().isEmpty()
+                || receipt.getItems() == null || receipt.getItems().isEmpty()
+                || receipt.getTotal() == null || receipt.getTotal().isEmpty()
+                || receipt.getPurchaseDate() == null || receipt.getPurchaseDate().isEmpty()
+                || receipt.getPurchaseTime() == null || receipt.getPurchaseTime().isEmpty() ){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(Map.of("message", "The receipt is invalid."));
+        }
+        for (Item item : receipt.getItems()) {
+            if (item.getShortDescription() == null || item.getShortDescription().isEmpty() ||
+                    item.getPrice() == null || item.getPrice().isEmpty()) {
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body(Map.of("message", "Each item in the receipt must have a short description and a price."));
+            }
         }
         return ResponseEntity.ok(receiptService.processReceipt(receipt));
     }
@@ -29,8 +42,8 @@ public class ReceiptController {
     @GetMapping("/receipts/{id}/points")
     public ResponseEntity<Object> getPoints(@PathVariable String id) {
         if (id == null || id.isEmpty() || id.trim().isEmpty()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                    .body(Map.of("message", "The receipt is invalid."));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of("message", "No receipt found for that ID"));
         }
         Map<String, Integer> points = receiptService.getPoints(id);
         if (points == null) {
